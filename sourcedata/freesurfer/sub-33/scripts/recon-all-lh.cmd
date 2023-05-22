6438ac78eecfbf96bd3560c88b4e06468d5cc6be
jbwexler <jbwexler@tutanota.com>
2023-05-22 13:17:21 -0500

    pre-push save (scratch)

--- sourcedata/freesurfer/sub-33/scripts/recon-all-lh.cmd
+++ sourcedata/freesurfer/sub-33/scripts/recon-all-lh.cmd
@@ -1,1 +1,1 @@
-/annex/objects/MD5E-s24888--ba6750954ab74d19bb3e9f1311b7493b.cmd
+../../../../.git/annex/objects/WZ/8G/MD5E-s24888--ba6750954ab74d19bb3e9f1311b7493b.cmd/MD5E-s24888--ba6750954ab74d19bb3e9f1311b7493b.cmd
